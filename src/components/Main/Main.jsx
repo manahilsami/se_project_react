ff1fe0ab1030178d60a4808515200265bb941581
Manahil Sami <manahil_sami@outlook.com>
2025-04-06 02:26:57 -0400

    ItemCards filter added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,7 +2,7 @@ import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 import { defaultClothingItems } from "../../utils/constants";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <main>
       <WeatherCard />
@@ -11,9 +11,13 @@ function Main() {
           Today is 75 &deg; F / You may want to wear:
         </p>
         <ul className="cards__list">
-          {defaultClothingItems.map((item) => {
-            return <ItemCard key={item._id} item={item} />;
-          })}
+          {defaultClothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
         </ul>
       </section>
     </main>
