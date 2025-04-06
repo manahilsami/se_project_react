b52f8ae741015172264f85013088cdbc7f1d0fe2
Manahil Sami <manahil_sami@outlook.com>
2025-04-06 01:51:26 -0400

    WeatherCard created & ItemCards rendered

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,23 @@
+import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
+
+function Main() {
+  return (
+    <main>
+      <WeatherCard />
+      <section className="cards">
+        <p className="cards__text">
+          Today is 75 &deg; F / You may want to wear:
+        </p>
+        <ul className="cards__list">
+          {defaultClothingItems.map((item) => {
+            return <ItemCard key={item._id} item={item} />;
+          })}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
