0b5b12c2d6b3174a780aaa4ed09483870492c31e
Manahil Sami <manahil_sami@outlook.com>
2025-04-10 15:21:54 -0400

    ItemModal opens/closes without image/styles

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,19 +1,13 @@
 import { act } from "react";
 import "./ModalWithForm.css";
 
-function ModalWithForm({
-  children,
-  buttonText,
-  title,
-  activeModal,
-  handleCloseClick,
-}) {
+function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
   return (
     <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
         <button
-          onClick={handleCloseClick}
+          onClick={onClose}
           type="button"
           className="modal__close"
         ></button>
