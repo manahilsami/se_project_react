05f14d1a3051b5acdc3f75da3c2c9054afd5fca8
Manahil Sami <manahil_sami@outlook.com>
2025-04-12 02:00:49 -0400

    ItemModal opens/closes with styles & OpenWeather API working

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -2,14 +2,18 @@ import "./ItemModal.css";
 
 function ItemModal({ activeModal, onClose, card }) {
   return (
-    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+    <div
+      className={`modal modal_item ${
+        activeModal === "preview" && "modal_opened"
+      }`}
+    >
       <div className="modal__content modal__content_type_image">
         <button
           onClick={onClose}
           type="button"
-          className="modal__close"
+          className="modal__close modal__close_item"
         ></button>
-        <img src="{card.link}" alt="" className="modal__image" />
+        <img src={card.link} alt={card.name} className="modal__image" />
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
           <p className="modal__weather">Weather: {card.weather}</p>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -36,3 +36,10 @@ export const defaultClothingItems = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
   },
 ];
+
+export const coordinates = {
+  latitude: 40.678177,
+  longitude: -73.94416,
+};
+
+export const APIkey = "1fc3898ca063c4be68bf19fe136e7a33";
