3af0eb7d440fd6bc504a5c40b0c162a6f9c848cb
Manahil Sami <manahil_sami@outlook.com>
2025-06-04 19:28:45 -0400

    overlay added to ItemModal

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -3,20 +3,26 @@ import "./ItemModal.css";
 function ItemModal({ activeModal, onClose, card }) {
   return (
     <div
-      className={`modal modal_item ${
-        activeModal === "preview" && "modal_opened"
+      className={`modal modal__overlay ${
+        activeModal === "preview" ? "modal_opened" : ""
       }`}
     >
-      <div className="modal__content modal__content_type_image">
-        <button
-          onClick={onClose}
-          type="button"
-          className="modal__close modal__close_item"
-        ></button>
-        <img src={card.link} alt={card.name} className="modal__image" />
-        <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
-          <p className="modal__weather">Weather: {card.weather}</p>
+      <div
+        className={`modal modal_item ${
+          activeModal === "preview" && "modal_opened"
+        }`}
+      >
+        <div className="modal__content modal__content_type_image">
+          <button
+            onClick={onClose}
+            type="button"
+            className="modal__close modal__close_item"
+          ></button>
+          <img src={card.link} alt={card.name} className="modal__image" />
+          <div className="modal__footer">
+            <h2 className="modal__caption">{card.name}</h2>
+            <p className="modal__weather">Weather: {card.weather}</p>
+          </div>
         </div>
       </div>
     </div>
