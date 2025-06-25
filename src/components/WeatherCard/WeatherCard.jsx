c7a7836cb7c968f5dfea500334cf19d28b18285a
Manahil Sami <manahil_sami@outlook.com>
2025-06-24 23:36:42 -0400

    farenheit/celsius conversion working

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,7 +1,11 @@
 import "./WeatherCard.css";
 import { weatherOptions } from "../../utils/constants";
+import { useContext } from "react";
+import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 
 function WeatherCard({ weatherData }) {
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+
   const filteredOptions = weatherOptions.filter((option) => {
     return (
       option.day === weatherData.isDay &&
@@ -14,7 +18,10 @@ function WeatherCard({ weatherData }) {
 
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
+      <p className="weather-card__temp">
+        {weatherData.temp[currentTemperatureUnit]} &deg;{" "}
+        {currentTemperatureUnit}
+      </p>
       <img
         src={weatherOptionUrl}
         alt={`${weatherOptionCondition}`}
