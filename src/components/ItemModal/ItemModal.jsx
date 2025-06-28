95e3c3498c02d0aa77e1be17ceede9f7414ae1d3
Manahil Sami <manahil_sami@outlook.com>
2025-06-28 01:57:54 -0400

    hotfix: onclick item  modal css

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -12,11 +12,11 @@ function ItemModal({ activeModal, onClose, card }) {
           activeModal === "preview" && "modal_opened"
         }`}
       >
-        <div className="modal__content modal__content_type_image">
+        <div className="item-modal__content modal__content_type_image">
           <button
             onClick={onClose}
             type="button"
-            className="modal__close modal__close_item"
+            className="modal__close_item"
           ></button>
           <img src={card.link} alt={card.name} className="modal__image" />
           <div className="modal__footer">
