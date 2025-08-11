15decb286b605de8589846c716187b712259bfbd
Manahil Sami <manahil_sami@outlook.com>
2025-08-11 01:36:27 -0400

    itemModal updated

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 import "./ItemModal.css";
 
 function ItemModal({ activeModal, onClose, onDelete, card }) {
+  const currentUser = useContext(CurrentUserContext);
+
+  const isOwn = currentUser && card.owner === currentUser._id;
+
   return (
     <div
       className={`modal modal__overlay ${
@@ -20,13 +26,15 @@ function ItemModal({ activeModal, onClose, onDelete, card }) {
           ></button>
           <img src={card.link} alt={card.name} className="modal__image" />
           <div className="modal__footer">
-            <button
-              onClick={onDelete}
-              type="button"
-              className="modal__delete_item"
-            >
-              Delete Item
-            </button>
+            {isOwn && (
+              <button
+                onClick={onDelete}
+                type="button"
+                className="modal__delete_item"
+              >
+                Delete Item
+              </button>
+            )}
             <h2 className="modal__caption">{card.name}</h2>
             <p className="modal__weather">Weather: {card.weather}</p>
           </div>
