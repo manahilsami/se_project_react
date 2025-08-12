f9c5dc20a5408bc3c1eeacb21347bc1e4f27ef9c
Manahil Sami <manahil_sami@outlook.com>
2025-08-12 18:23:19 -0400

    fix: sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -8,32 +8,38 @@ function Sidebar({ handleEditProfileClick, handleSignOut }) {
 
   return (
     <div className="sidebar">
-      {currentUser?.avatar ? (
-        <img 
-          className="sidebar__avatar" 
-          src={currentUser.avatar} 
-          alt={currentUser.name} 
-        />
-      ) : (
-        <div className="sidebar__avatar-placeholder">
-          {currentUser?.name?.charAt(0).toUpperCase()}
-        </div>
-      )}
-      <p className="sidebar__username">{currentUser?.name}</p>
-      <button 
-        className="sidebar__edit-button"
-        onClick={handleEditProfileClick}
-        type="button"
-      >
-        Change profile data
-      </button>
-      <button 
-        className="sidebar__signout-button"
-        onClick={handleSignOut}
-        type="button"
-      >
-        Sign out
-      </button>
+      <div className="sidebar__user-info">
+        {currentUser?.avatar ? (
+          <img
+            className="sidebar__avatar"
+            src={currentUser.avatar}
+            alt={currentUser.name}
+          />
+        ) : (
+          <div className="sidebar__avatar-placeholder">
+            {currentUser?.name?.charAt(0).toUpperCase()}
+          </div>
+        )}
+        <p className="sidebar__username">{currentUser?.name}</p>
+      </div>
+
+      <div className="sidebar__buttons">
+        <button
+          className="sidebar__edit-button"
+          onClick={handleEditProfileClick}
+          type="button"
+        >
+          Change profile data
+        </button>
+
+        <button
+          className="sidebar__signout-button"
+          onClick={handleSignOut}
+          type="button"
+        >
+          Sign out
+        </button>
+      </div>
     </div>
   );
 }
