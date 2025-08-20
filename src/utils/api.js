edae3acc8211743da251b5bcc8033f886835a9ee
Manahil Sami <manahil_sami@outlook.com>
2025-08-19 23:24:16 -0400

    updated frontend for server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwrms.jumpingcrab.com"
+    : "http://localhost:3001";
 
 function checkResponse(res) {
   if (res.ok) {
